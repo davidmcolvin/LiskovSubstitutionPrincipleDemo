dbbe35735c824214b5033b5bc480830162d72ca5
davidcol1 <davidmcolvin@gmail.com>
2019-12-02 14:11:29 -0500

    Initial commit of Liskov Substitution Principle Demo.  Principal is not applied yet

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using DemoLibrary;
+
+namespace ConsoleUI
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Manager accountingVP = new Manager();
+
+      accountingVP.FirstName = "Emma";
+      accountingVP.LastName = "Stone";
+      accountingVP.CalculatePerHourRate(4);
+
+      Employee emp = new CEO();
+
+      emp.FirstName = "Tim";
+      emp.LastName = "Corey";
+      emp.AssignManager(accountingVP);
+      emp.CalculatePerHourRate(2);
+
+      Console.WriteLine($"{emp.FirstName}'s salary is ${emp.Salary}/hour.");
+
+      Console.ReadLine();
+    }
+  }
+}
---
+++ DemoLibrary/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+  public class Employee
+  {
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public decimal Salary { get; set; }
+
+    public Employee Manager { get; set; }
+
+    public virtual void CalculatePerHourRate(int rank)
+    {
+      decimal baseAmount = 12.50M;
+
+      Salary = baseAmount + (rank * 2);
+    }
+
+    public virtual void AssignManager(Employee manager)
+    {
+      // Simulate doing other tasks here - otherwise, this should be
+      // a property set statement, not a method
+      Manager = manager;
+    }
+
+  }
+}
